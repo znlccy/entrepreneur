1b129e0ddb937c10660608fa4a6f49c57ae68a5c
znlccy <756311868@qq.com>
2018-11-20 19:45:58 +0800

    添加轮播详情

---
+++ application/admin/validate/Permission.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/11/20
+ * Time: 19:42
+ * Comment: 权限验证器
+ */
+
+namespace app\admin\validate;
+
+class Permission extends BasisValidate {
+
+}
